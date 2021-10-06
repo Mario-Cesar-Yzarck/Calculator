151f1203494603157cb3231e34f2bc3fe51138d6
Mario <yzarckwow@gmail.com>
2021-10-06 16:38:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+;(function (win, doc) {
+  'use strict'
+  /*
+Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+As regras são:
+
+- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+diretamente;
+- O input deve iniciar com valor zero;
+- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+multiplicação(x) e divisão(÷);
+- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+que irá limpar o input, deixando-o com valor 0;
+
+- A cada número pressionado, o input deve atualizar concatenando cada valor
+digitado, como em uma calculadora real;
+- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+operação no input. Se o último caractere no input já for um símbolo de alguma
+operação, esse caractere deve ser substituído pelo último pressionado.
+Exemplo:
+- Se o input tem os valores: "1+2+", e for pressionado o botão de
+multiplicação (x), então no input deve aparecer "1+2x".
+- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+input;
+- Ao pressionar o botão "CE", o input deve ficar zerado.
+*/
+  const $btnNumPress = doc.querySelectorAll("[data-js='btnNumber']")
+  const $btnOperator = doc.querySelectorAll("[data-js='btnOperation']")
+  const $btnIgual = doc.querySelector("[data-js='btnIgual']")
+  const $btnReset = doc.querySelector("[data-js='btnReset']")
+  var $input = doc.querySelector('input')
+  $input.value = 0
+
+  $btnNumPress.forEach(function (item, index) {
+    $btnNumPress[index].addEventListener(
+      'click',
+      function (e) {
+        if ($input.value == 0) {
+          $input.value = ''
+        }
+        $input.value = $input.value.concat(item.value)
+      },
+      false
+    )
+  })
+
+  $btnOperator.forEach(function (item, index) {
+    $btnOperator[index].addEventListener(
+      'click',
+      function (e) {
+        let operator = $input.value[$input.value.length - 1]
+
+        if (
+          operator === '+' ||
+          operator === '-' ||
+          operator === '*' ||
+          operator === '/'
+        ) {
+          if (item.value === '(') {
+            $input.value = $input.value.concat(item.value)
+          } else {
+            $input.value = $input.value.replace(/[\/\-+*]$/g, item.value)
+            $input.value = $input.value
+          }
+        } else {
+          $input.value = $input.value.concat(item.value)
+        }
+      },
+      false
+    )
+  })
+
+  $btnIgual.addEventListener(
+    'click',
+    function (e) {
+      $input.value = eval($input.value)
+    },
+    false
+  )
+})(window, document)
